485a11275586992d8987aace49f36d13346df6ab
Carlos Muñoz <carlos.damian.munoz@gmail.com>
2021-12-16 19:01:51 -0300

    Update media images and version release

--- playblast/__init__.py
+++ playblast/__init__.py
@@ -15,7 +15,7 @@ bl_info = {
     "name": "Playblast",
     "author": "carlosmu <carlos.damian.munoz@gmail.com>",
     "blender": (2, 83, 0),
-    "version": (1, 2, 8),
+    "version": (1, 3, 0),
     "category": "Animation",
     "location": "3D View Main Menu and/or Right Click Context Menu",
     "description": "Improves viewport render animation user experience",
